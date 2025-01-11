61bc8f00b38bebd383a99215f272528646832a79
bibek-1-bit <bibekbasnet687@gmail.com>
2025-01-11 20:51:03 +0545

    counter.sh

--- some codes/counter.sh
+++ some codes/counter.sh
@@ -5,10 +5,11 @@ echo "A simple time counter"
 echo "choose number to set the counter"
 read num
 echo "set counting order => Ascending or Descending"
+echo "choose Asc for Ascending or Dsc for Descending"
 read order
 
 
-if [ $order == Ascending ]; then
+if [ $order == Asc ]; then
 for i in $(seq 1 $num); do
 echo "$i"
 sleep 1

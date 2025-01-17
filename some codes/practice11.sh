9e3f75030dc0125c50ef3c92a1f59f6280a21bce
bibek-1-bit <bibekbasnet687@gmail.com>
2025-01-17 18:46:12 +0545

    practice11.sh

--- some codes/practice11.sh
+++ some codes/practice11.sh
@@ -4,4 +4,4 @@ echo "counting the number of files in current folder"
 
 files_count=$(ls -p | grep -v / | wc -l)
 
-echo "The total number of files in this directory is: $files_count"
+echo "The total number of files in this directory is : $files_count"
